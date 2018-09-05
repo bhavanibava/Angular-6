de207e758de8dc1945247740309b06e27d324644
bhavanibava <39649592+bhavanibava@users.noreply.github.com>
2018-09-05 22:43:10 +0530

    Add files via upload

---
+++ basic project angular/src/app/mycomponent/mycomponent.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'mycomponent',
+  templateUrl: './mycomponent.component.html',
+  styleUrls: ['./mycomponent.component.css']
+})
+export class MycomponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+
+  }
+
+}
